8dacfcc2df103f78d145a0d4848c68e4aacf41a6
Woo Han <hanwj100@gmail.com>
2022-08-17 14:41:18 -0700

    Create helper function to call math functions

--- scripts.js
+++ scripts.js
@@ -13,3 +13,16 @@ function multiply(x, y) {
 function divide(x, y) {
     return x/y;
 }
+
+function operate(operator, x, y) {
+    switch(operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+    }
+}
