55bad537aa2ea74a58f8d22198a6e52c56da615b
Woo Han <hanwj100@gmail.com>
2022-08-17 14:34:44 -0700

    Simplify math functions to only take two args
    Initially I created the math functions to take multiple arguments and
    reduce the array of arguments into a single return value but I will
    use a helper function to call these functions individually.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,15 @@
-function add(...nums) {
-    return nums.reduce(((total, num) => (total += num)), 0);
+function add(x, y) {
+    return x + y;
 }
 
-function subtract(...nums) {
-    return nums.reduce(((total, num) => (total -= num)), 0);
+function subtract(x, y) {
+    return x - y;
 }
 
-function multiply(...nums) {
-    if (nums.length == 0) {
-        console.log("Please enter at least one number")
-    }
-    else {
-        return nums.reduce(((total, num) => (total *= num)));
-    }
+function multiply(x, y) {
+    return x * y;
 }
 
-function divide(...nums) {
-    if (nums.length == 0) {
-        console.log("Please enter at least one number")
-    }
-    else {
-        return nums.reduce(((total, num) => (total /= num)));
-    }
+function divide(x, y) {
+    return x/y;
 }
